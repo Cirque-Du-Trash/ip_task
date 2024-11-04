a38491bcd7c6f43024d9844a0474a6b99fac223b
mr-cliche-git <mr.cliche7@gmail.com>
2024-11-04 11:12:19 +0900

    Updates

---
+++ precache-manifest.190cddb8fee6fd090f29f5bce98365b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06bc4b3555c50e3eaca7478876276e6",
+    "url": "/Cirque-Du-Trash/ip_task.git/index.html"
+  },
+  {
+    "revision": "43d64f7796a115d0d72c",
+    "url": "/Cirque-Du-Trash/ip_task.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/Cirque-Du-Trash/ip_task.git/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/Cirque-Du-Trash/ip_task.git/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "43d64f7796a115d0d72c",
+    "url": "/Cirque-Du-Trash/ip_task.git/static/js/main.d4224337.chunk.js"
+  },
+  {
+    "revision": "7d9d6c773de3221a4cb3",
+    "url": "/Cirque-Du-Trash/ip_task.git/static/js/runtime~main.441ac969.js"
+  }
+]);
